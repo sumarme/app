d1f2d6241a0af756f4b75625ca791b5d6eb741bc
gcuervo <cuervogaston@gmail.com>
2015-08-22 01:54:56 -0300

    falta

--- app.js
+++ app.js
@@ -18,19 +18,21 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);   // lo que digo es que todas las rutas van montadas en el root
+app.use('/', routes); // lo que digo es que todas las rutas van montadas en el root
 app.use('/users', users);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -38,24 +40,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 var mongoUtils = require('../lib/mongoUtils.js');
-var db = mongoUtils.getDB();
+var geoPosition = require('../lib/geoPosition.js');
+
+
+
+//var db = mongoUtils.getDB();
+
+/*var dbH = [{
+    users = {
+        "usuario1": {
+            "nombre": "mayor tom",
+            "edad": "23",
+            "puesto": "delantero",
+            "foto": "123"
+        },
+        "usuario1": {
+            "nombre": "kirk",
+            "edad": "26",
+            "puesto": "delantero",
+            "foto": "124"
+        },
+        "usuario1": {
+            "nombre": "clark",
+            "edad": "28",
+            "puesto": "delantero",
+            "foto": "124"
+        }
+    }
+}]*/
 
 
 
@@ -19,25 +46,62 @@ router.get('/registrate', function(req, res, next) {
     });
 });
 
-router.get('/seleccion', function(req, res, next) {
+/*if (geoPosition.init()) { // Geolocation Initialisation
+    geoPosition.getCurrentPosition(success_callback, error_callback, {
+        enableHighAccuracy: true
+    });
+} else {
+    // You cannot use Geolocation in this device
+}
+geoPositionSimulator.init();
 
-    var filter = {
-        disponible: true
-    };
+function success_callback(p) {
+            // p.latitude : latitude value
+            // p.longitude : longitude value
+}
 
-    db.collection('users').findOne(filter, function(err, user) {
+// p : geolocation object
 
-        if (err) {
-            // template de errores
-            return res.render('error', {
-                error: err
+function error_callback(p) {
+    // p.message : error message
+}*/
+
+
+
+mongoUtils.connectToServer(function(err, db) {
+    if (err) {
+        console.log(err);
+    }
+
+    router.get('/seleccion', function(req, res, next) {
+
+        var filter = {
+            disponible: true
+        };
+
+
+        db.collection('users').findOne({}, function(err, userP) {
+            if (userP) {
+                console.log(userP.nombre);
+            } else {
+                console.log('no data for this');
+
+            }
+
+            if (err) {
+                // template de errores
+                return res.render('error', {
+                    error: err
+                });
+            }
+
+
+            res.render('seleccion', {
+                title: 'Selecciones',
+                user: userP
             });
-        }
-        res.render('seleccion', {
-            title: 'Selecciones',
-            user: user
+            db.close();
         });
-
     });
 
 });
