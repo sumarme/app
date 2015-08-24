3a47d2e452d8bdcabf0e532541fbad35b74ec150
Facundo Gomez <facundogomez15@gmail.com>
2015-08-24 20:33:37 -0300

    Login de FB agregado

--- app/public/assets/js/ingresa.js
+++ app/public/assets/js/ingresa.js
@@ -1,58 +1,33 @@
-function redirijir() {
-    FB.getLoginStatus(function(response) {
-        console.log(response)
+$(document).ready(function() {
+    function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response)
+        })
+    }
+
+    FB.init({
+        appId: '501734716657911',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.4'
     })
-}
 
-//le robé el waitingDialog a a0viedo
-var waitingDialog = (function($) {
-    // Creating modal dialog's DOM
-    var $dialog = $(
-        '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
-        '<div class="modal-dialog modal-m">' +
-        '<div class="modal-content">' +
-        '<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
-        '<div class="modal-body">' +
-        '<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
-        '</div>' +
-        '</div></div></div>');
-
-    return {
-        /**
-         * Opens our dialog
-         * @param message Custom message
-         * @param options Custom options:
-         *                options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
-         *                options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
-         */
-        show: function(message, options) {
-            // Assigning defaults
-            var settings = $.extend({
-                dialogSize: 'm',
-                progressType: ''
-            }, options);
-            if (typeof message === 'undefined') {
-                message = 'Loading';
+    $('#fbLogin').click(function() {
+        FB.login(function(response) {
+            if (response.status == 'connected') {
+                window.location.replace('/perfil');
+                // console.log(response.authResponse.accessToken);
+                // aca poner una funcion, definida despues, que llame a 
+                // FB.api y haga console.log de los que tiene el access.Token
+            } else if (response.status == 'not_authorized') {
+                //esta logeado en FB pero no en la app
+                document.getElementById('status').innerHTML = 'Por favor inicia sesion en SUMARME.';
+            } else {
+                //no esta logeado en FB
+                document.getElementById('status').innerHTML = 'Por favor inicia sesion en Facebook.';
             }
-            if (typeof options === 'undefined') {
-                options = {};
-            }
-            // Configuring dialog
-            $dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
-            $dialog.find('.progress-bar').attr('class', 'progress-bar');
-            if (settings.progressType) {
-                $dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
-            }
-            $dialog.find('h3').text(message);
-            // Opening dialog
-            $dialog.modal();
-        },
-        /**
-         * Closes dialog
-         */
-        hide: function() {
-            $dialog.modal('hide');
-        }
-    };
-
-})(jQuery);
+        }, {
+            scope: 'public_profile,email, user_birthday'
+        });
+    });
+});
